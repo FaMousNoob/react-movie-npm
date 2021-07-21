4740c723ec5d6a767c4dc954d97c5bf18a52305d
P.Tao <ngobaophuongtao@gmail.com>
2021-07-21 08:33:18 +0700

    fix errors

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // --B4--
-import '../node_modules/jquery/dist/jquery';
-import '../node_modules/popper.js/dist/popper';
-import '../node_modules/bootstrap/dist/js/bootstrap';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import "../node_modules/jquery/dist/jquery";
+import "../node_modules/popper.js/dist/popper";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 // REDUX
 import {Provider} from 'react-redux';
